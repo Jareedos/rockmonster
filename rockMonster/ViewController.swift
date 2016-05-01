92ee3416e2ad22bd4a020141562c3179c165af6e
Jareedos <jaredallens@gmail.com>
2016-05-01 13:55:32 -0700

    Updated View to change for landscaped screens

--- rockMonster/ViewController.swift
+++ rockMonster/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var monsterImg: UIImageView!
+    @IBOutlet weak var foodImg: UIImageView!
+    @IBOutlet weak var heartImg: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        var imgArray = [UIImage]()
+
+        var x = 1
+        while x <= 4 {
+            let img = UIImage(named: "idle(\(x)).png")
+            imgArray.append(img!)
+            x += 1
+        }
+        
+        monsterImg.animationImages = imgArray
+        monsterImg.animationDuration = 0.8
+        monsterImg.animationRepeatCount = 0
+        monsterImg.startAnimating()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        print("I Just touched the screen!")
     }
-
-
 }
+    
+
+
+
 
