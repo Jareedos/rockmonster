6ac8dcaa914f508e0ee4716c100b48e56fe09819
Jareedos <jaredallens@gmail.com>
2016-05-01 18:48:18 -0700

    added skull variables and set skulls to be less visable

--- rockMonster/ViewController.swift
+++ rockMonster/ViewController.swift
@@ -13,16 +13,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var monsterImg: MonsterImg!
     @IBOutlet weak var foodImg: DragImg!
     @IBOutlet weak var heartImg: DragImg!
-
+    @IBOutlet weak var penalty1Img: UIImageView!
+    @IBOutlet weak var penalty2Img: UIImageView!
+    @IBOutlet weak var penalty3Img: UIImageView!
+    
+    let DIM_ALPHA: CGFloat = 0.2
+    let OPAQUE: CGFloat = 1.0
+    let MAX_PENALTIES = 3
+    
+    var penalties = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        foodImg.dropTarget = monsterImg
+        heartImg.dropTarget = monsterImg
+        
+        penalty1Img.alpha = DIM_ALPHA
+        penalty2Img.alpha = DIM_ALPHA
+        penalty3Img.alpha = DIM_ALPHA
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.itemDroppedOnCharacter(_:)), name: "onTargetDropped", object: nil)
        
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        print("I Just touched the screen!")
+    func itemDroppedOnCharacter(notif: AnyObject) {
+        print("Item Dropped On Character")
     }
+   
 }
     
 
